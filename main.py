bb8367f5dcaba003f5294b7a79445fc10728d58f
Eirik Humlen <ehumlen@protonmail.com>
2024-01-14 23:24:22 +0100

    new master

--- main.py
+++ main.py
@@ -4,70 +4,85 @@ import glob
 
 import matplotlib.pyplot as plt
 import pandas as pd
-import statsmodels.api as sm
 import yfinance as yf
+from statsmodels import api as sm
 from tqdm import tqdm
 
-# Import data into dataframes
+#TODO: Rewrite to Polars
+
+# Local data library
 FOLDER_PATH = (
     r"C:\Users\eirik\Codebase\001 Compounding Linear Relationships\data\sample data"
 )
 all_files = glob.glob(FOLDER_PATH + "/*.csv")
 
-# Create empty lists
+# Prepare structures
 Ratios = []
+Multiples = []
 Stocks = []
+Prices = []
 
-
-
-# Ratios
+# Put data in dfs
 for filename in tqdm(all_files):
+    # Determine what kind of file it is
     name = filename.split("sample data\\")[1].split(".csv")[0]
     if "ratios" in name:
         df = pd.read_csv(filename, index_col=None, header=0)
         ticker = name.split("ratios_")[1]
         df["Ticker"] = ticker
         Ratios.append(df)
+    if "multiples" in name:
+        df = pd.read_csv(filename, index_col=None, header=0)
+        ticker = name.split("multiples_")[1]
+        df["Ticker"] = ticker
+        Multiples.append(df)
         Stocks.append(ticker)
     else:
-        pass
+        ticker = name
+    Stocks.append(ticker)
 
+df_ratios = pd.concat(Ratios, axis=0, ignore_index=True)
+df_multiples = pd.concat(Multiples, axis=0, ignore_index=True)
 
-# Create a dataframe with dates from 2013-12-01 to today
+# Create date table
 df_dates = pd.DataFrame(
     pd.date_range(start="2013-12-01", end=datetime.datetime.today(), freq="D"),
     columns=["Date"],
 )
 
-df_ratio = pd.concat(Ratios, axis=0, ignore_index=True)
-
-# Remove x from all rows in dataframe
-f = [c for c in df_ratio.columns if c not in ["Ticker"]]
-df_ratio[f] = (
-    df_ratio[f]
+# Format dfs to raw nums
+r = [c for c in df_ratios.columns if c not in ["Ticker"]]
+df_ratios[r] = (
+    df_ratios[r]
     .replace("x", "", regex=True)
     .replace(r"\)", "", regex=True)
     .replace(r"\(", "-", regex=True)
 )
 
-# Format dates
-df_ratio["Date"] = pd.to_datetime(df_ratio["Date"], format="%d/%m/%y")
-df_ratio["Date 1Y"] = df_ratio["Date"] + datetime.timedelta(days=365)
-
-Prices = []
-
-## Create a dataframe with dates from 2013-12-01 to today
-df_dates = pd.DataFrame(
-    pd.date_range(start="2013-12-01", end=datetime.datetime.today(), freq="D"),
-    columns=["Date"],
+# BUG: Highlighting thinks parentheses within strings are counted
+m = [c for c in df_multiples.columns if c not in ["Ticker"]]
+df_multiples[m] = (
+    df_multiples[m]
+    .replace("x", "", regex=True)
+    .replace(r"\)", "", regex=True)
+    .replace(r"\(", "-", regex=True)
 )
+
+# Date formatting
+df_ratios["Date"] = pd.to_datetime(df_ratios["Date"], format="%d/%m/%y")
+df_ratios["Date 1Y"] = df_ratios["Date"] + datetime.timedelta(days=365)
+df_multiples["Date"] = pd.to_datetime(df_multiples["Date"], format="%d/%m/%y")
+df_multiples["Date 1Y"] = df_multiples["Date"] + datetime.timedelta(days=365)
 
 # Import stock prices
 for symbol in tqdm(Stocks):
     df_price = yf.Ticker(symbol).history(start="2013-12-01")
     df_price["Date"] = df_price.index
     df_price = df_price.reset_index(drop=True)
-    df_price["Date"] = df_price["Date"].dt.tz_convert(None).dt.round("D")
+    try:
+        df_price["Date"] = df_price["Date"].dt.tz_convert(None).dt.round("D")
+    except:
+        pass
     df_price["Ticker"] = symbol
     df_price = df_price[["Date", "Ticker", "Close"]]
     df_price = pd.merge(
@@ -87,16 +102,26 @@ for symbol in tqdm(Stocks):
 df_prices = pd.concat(Prices, axis=0)
 
 # Merge dataframes
-df = pd.merge(
-    df_ratio,
+df_fact = pd.merge(
+    df_ratios,
+    df_multiples,
+    how="outer",  # no particular reason why left, mby inner?
+    left_on=["Date", "Ticker"],
+    right_on=["Date", "Ticker"],
+    suffixes=("", "_RATIOS"),
+)
+
+df_fact = pd.merge(
+    df_fact,
     df_prices,
     how="left",
     left_on=["Date", "Ticker"],
     right_on=["Date", "Ticker"],
     suffixes=("", ""),
 )
-df = pd.merge(
-    df,
+
+df_fact = pd.merge(
+    df_fact,
     df_prices,
     how="left",
     left_on=["Date 1Y", "Ticker"],
@@ -104,15 +129,10 @@ df = pd.merge(
     suffixes=("", "_1Y"),
 )
 
-df = df.drop(["Date_1Y"], axis=1)
-df = df.rename(columns={"Close_1Y": "Close 1Y"})
-
-# Calculate 1Y return
-df["Return 1Y"] = (df["Close 1Y"] - df["Close"]) / df["Close"] * 100
-
-# Rename Return on Assets % Column
-df = df.rename(
+# Fix, rename and remove columns
+df_fact = df_fact.rename(
     columns={
+        # Ratios
         "Return on Assets %\xa0": "ROA",
         "Return on Capital %\xa0": "ROC",
         "Return On Equity %\xa0": "ROE",
@@ -122,18 +142,22 @@ df = df.rename(
         "Total Liabilities / Total Assets\xa0": "Liabilities/Assets",
         "Current Ratio\xa0": "Current Ratio",
         "Quick Ratio\xa0": "Quick Ratio",
+        # Multiples
+        "NTM Revenues\xa0": "NTM Revenues",
+        "NTM Normalized Earnings Per Share\xa0": "NTM EPS",
+        "LTM Diluted EPS Before Extra\xa0": "LTM EPS",
+        "NTM Price / Sales (P/S)": "NTM P/S",
+        "NTM Price / Normalized Earnings (P/E)": "NTM P/E",
+        "LTM Price / Sales (P/S)": "LTM P/S",
+        "LTM Price / Diluted EPS (P/E)\xa0": "LTM P/E",
     }
 )
 
-# REMOVE IN PROD
-print(list(df.columns.values))
+df_fact["NTM/LTM Revenue"] = df_fact["NTM Revenues"] / df_fact["LTM Revenues"]
+df_fact["NTM/LTM EPS"] = df_fact["NTM EPS"] / df_fact["LTM EPS"]
+df_fact["Return 1Y"] = (df_fact["Close_1Y"] - df_fact["Close"]) / df_fact["Close"] * 100
 
-# Convert to numeric
-df["Current Ratio"] = pd.to_numeric(df["ROA"], errors="coerce")
-df["Quick Ratio"] = pd.to_numeric(df["ROC"], errors="coerce")
-
-# Select key columns
-df = df[
+df_fact = df_fact[
     [
         "Date",
         "Ticker",
@@ -147,22 +171,30 @@ df = df[
         "Liabilities/Assets",
         "Current Ratio",
         "Quick Ratio",
+        "NTM P/S",
+        "NTM P/E",
+        "LTM P/S",
+        "LTM P/E",
+        "NTM Revenues",
+        "LTM Revenues",
+        "NTM EPS",
+        "LTM EPS",
+        "NTM/LTM Revenue",
+        "NTM/LTM EPS",
     ]
 ]
 
-# REMOVE IN PROD
-print(df.info())
-
-# Select Metric
+# Select metric
 METRIC = "ROE"
 
-# Prepare data
-df = df[df[f"{METRIC}"] > 0]
-df = df[df[f"{METRIC}"] < 1]
-df = df.dropna()
-x = df[f"{METRIC}"]
-y = df["Return 1Y"]
+# TODO: Scale metric
 
+# Prep data for OLS print
+df_fact = df_fact[df_fact[f"{METRIC}"] > 0]
+df_fact = df_fact[df_fact[f"{METRIC}"] < 1]
+df_fact = df_fact.dropna(subset=[f"{METRIC}", "Return 1Y"])
+x = df_fact[f"{METRIC}"]
+y = df_fact["Return 1Y"]
 
 # Plot data
 model = sm.OLS(y, x).fit()
@@ -173,12 +205,11 @@ pred_ols = model.get_prediction()
 iv_l = pred_ols.summary_frame()["obs_ci_lower"]
 iv_u = pred_ols.summary_frame()["obs_ci_upper"]
 
-fig, ax = plt.subplots(figsize=(8, 6))
-ax.plot(x, y, "o", label="data")
-ax.plot(x, model.fittedvalues, "r--.", label="OLS")
-ax.plot(x, iv_u, "r--")
-ax.plot(x, iv_l, "r--")
-ax.legend(loc="best")
+# Ignore type errors in plot
+fig, ax = plt.subplots()
+ax.plot(x, y, "o", label="data")  # type: ignore
+ax.plot(x, model.fittedvalues, "r--.", label="OLS")  # type: ignore
+ax.plot(x, iv_u, "r--")  # type: ignore
+ax.plot(x, iv_l, "r--")  # type: ignore
+ax.legend(loc="best")  # type: ignore
 plt.show()
-
-# TODO: Add multiples
